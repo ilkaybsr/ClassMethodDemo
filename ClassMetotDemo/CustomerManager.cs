47da468d9b9137a3261246bc0a7b610df72fdf7f
ilkaybsraydin@hotmail.com <ilkaybsraydin@hotmail.com>
2021-02-02 15:46:59 +0300

    InitialCommit

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+     class CustomerManager
+    {
+        List<Customer> customers = new List<Customer>();
+        
+
+        public void AddCustomer(Customer customer) 
+        {
+            if (customer.IdNumber > 0==true)
+            {
+                customers.Add(customer);
+                Console.WriteLine("Müşteri Sisteme Eklendi");
+            }
+
+            else
+            {
+                Console.WriteLine("Müşteri Sisteme Eklenemedi!");
+            }
+        }
+
+        public List<Customer> GetList()
+        {
+            return customers;
+        }
+
+        public void Delete ( int Id) 
+        {
+            for (int i = 0; i < customers.Count; i++)
+            {
+                if (customers[i].IdNumber == Id)
+                    customers.Remove(customers[i]);
+                Console.WriteLine("Müşteri Sistemdeen Siilindi");
+            }
+        }
+        
+           
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.IdNumber = 1;
+            customer1.FirstName = "İlkay";
+            customer1.LastName = "AYDIN";
+            customer1.City = "ANKARA";
+
+            Customer customer2 = new Customer();
+            customer2.IdNumber = 2;
+            customer2.FirstName = "Batuhan";
+            customer2.LastName = "ÖZYÖN";
+            customer2.City = "ANKARA";
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.AddCustomer(customer1);
+
+            Customer[] customers = new[] {customer1,customer2};
+
+            foreach (Customer customer in customers)
+            {
+                System.Console.WriteLine(customer.IdNumber + " " + customer.FirstName + "  " + customer.LastName + " " + "/" + customer.City);
+            }
+
+            
+            
+        }
+    }
+}
